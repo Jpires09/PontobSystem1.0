6d6c5d5f7443d526652b8702bfa0d05d19ed0715
Jpires09 <jeancpires09@gmail.com>
2024-07-24 21:20:49 -0300

    simplified routes for better understanding

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
   
-  resources :products, only: [:show, :new, :create, :edit, :update, :destroy]
-  get 'products/', to: 'products#index'
-  
-  get 'employees/', to: 'employees#index'
-  resources :employees, only: [:show, :new, :create, :edit, :update, :destroy]
-
-  get 'clients/', to: 'clients#index'
-  resources :clients, only: [:show, :new, :create, :edit, :update, :destroy]
-
+  resources :products, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :employees, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :clients, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
