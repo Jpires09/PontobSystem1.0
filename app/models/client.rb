92272422d754be0d1be3a01d98eac88316fa431e
Jpires09 <jeancpires09@gmail.com>
2024-08-02 14:32:48 -0300

    added changes necessary to associate both models trhough enrollments table

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,4 @@
 class Client < ApplicationRecord
+  has_many :enrollments
+  has_many :turmas, through: :enrollments
 end
--- app/models/turma.rb
+++ app/models/turma.rb
@@ -1,4 +1,6 @@
 class Turma < ApplicationRecord
   belongs_to :semanal, optional: true 
   belongs_to :professor_titular, class_name: 'Employee', foreign_key: 'professor_titular_id'
+  has_many :enrollments
+  has_many :clients, through: :enrollments
 end
