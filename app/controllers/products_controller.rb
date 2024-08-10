fed502b0ef55a6caefbac4471a1fa12c83302d05
Jpires09 <jeancpires09@gmail.com>
2024-08-10 12:02:25 -0300

    fixed new params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price_cents)
+    params.require(:product).permit(:name, :description, :price)
   end
 end
