c64a576c1c0098d4ef04e96de52d97a982a66307
Jpires09 <jeancpires09@gmail.com>
2024-08-20 20:11:14 -0300

    Now clients has many attendances to groups Created add_credits function witch will be called every new plan/month bought

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,10 @@
 class Client < ApplicationRecord
+  has_many :attendances
   has_many :enrollments
   has_many :groups, through: :enrollments
+
+  def add_credits(plan)
+    self.credits += plan.credits
+    save
+  end
 end
